b7a3d618c83d3da96506fc34dd48c5834679cd40
Dharini Raghavan <dhariniraghavan2001@gmail.com>
2024-03-11 11:43:18 +0530

    Update pose_correction.py

--- pose_correction.py
+++ pose_correction.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 def transform_covariance(T, covariance):
-    return np.dot(T, np.dot(covariance, T.T))
+    return np.dot(T, np.dot(covariance, T.T)) #T is the transformation (relative pose) between robot_A and robot_B
 
 def reduce_covariance(robot_B_covariance, transformed_robot_A_covariance, alpha):
-    reduced_covariance = robot_B_covariance - alpha * transformed_robot_A_covariance
+    reduced_covariance = robot_B_covariance - alpha * transformed_robot_A_covariance #alpha is user defined hyper-parameter that determines the magnitude of correction to be applied.
     return reduced_covariance
